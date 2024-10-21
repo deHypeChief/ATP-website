95fba69c143dcdfadd9178b73304f58814e6c78e
deHypeChief <dev.hype7@gmail.com>
2024-10-21 06:18:54 +0100

    update cms host

--- cms/sanity.cli.js
+++ cms/sanity.cli.js
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: 'jaqodarg',
     dataset: 'production'
   },
+  studioHost: "atpcms",
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
