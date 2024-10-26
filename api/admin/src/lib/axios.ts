ae844c21f2e5848a781047da2908d85f174630a6
dehypechief <dev.hype7@gmailcom>
2024-10-26 08:42:05 -0700

    server and api setup

--- api/admin/src/lib/axios.ts
+++ api/admin/src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // Replace with your backend URL
+  baseURL:  import.meta.env.VITE_SERVER_API, // Replace with your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
--- client/src/libs/api/axios.config.js
+++ client/src/libs/api/axios.config.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', 
+  baseURL: import.meta.env.VITE_SERVER_API, 
   headers: {
     'Content-Type': 'application/json',
   },
